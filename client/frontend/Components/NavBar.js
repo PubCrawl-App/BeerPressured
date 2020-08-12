c12987ea9d1ec180f5a703d7b00dd2e65c247bb8
Silvia Miranda <silvia.kemp@gmail.com>
2020-08-12 19:01:54 -0400

    navbar update

--- client/frontend/Components/NavBar.js
+++ client/frontend/Components/NavBar.js
@@ -1,20 +1,19 @@
-import React from "react";
-import {NavLink, BrowserRouter, Redirect} from 'react-router-dom'; 
+import React from 'react';
+import { NavLink, BrowserRouter, Redirect } from 'react-router-dom';
 
 const NavBar = () => {
-    return (
-      <div className="navbar-container">
-          {/* <a className="nav-btn" href="/home">HOME</a>
+  return (
+    <div className="navbar-container">
+      {/* <a className="nav-btn" href="/home">HOME</a>
           <a className="nav-btn" href="/create">CREATE CRAWL</a>
           <a className="nav-btn" href="#">MAP</a>
           <a className="nav-btn" href="#">PROFILE</a> */}
-          {/* <BrowserRouter> */}
-            <NavLink to='/createHome'>CREATE CRAWL</NavLink>
-            <NavLink to='/home'>See crawl List</NavLink>
-          {/* </BrowserRouter> */}
-      </div>
-    );
+      {/* <BrowserRouter> */}
+      <NavLink to="/createHome">CREATE CRAWL</NavLink>
+      <NavLink to="/home">See crawl List</NavLink>
+      {/* </BrowserRouter> */}
+    </div>
+  );
+};
 
-}
- 
 export default NavBar;
