1748851c9ac906b9964cc3bf12482ab1e91853cb
Silvia Miranda <silvia.kemp@gmail.com>
2020-08-13 14:00:38 -0400

    finalized oauth cookie flow

--- client/frontend/Components/LoginButton.js
+++ client/frontend/Components/LoginButton.js
@@ -5,29 +5,6 @@ import { useGoogleLogin } from 'react-google-login';
 const clientId = '1056890442611-8hj0b6phoo8k6kpd0a532gc1f63sq4eo.apps.googleusercontent.com';
 
 const LoginButton = () => {
-  // const [email, setEmail] = useState('');
-  // const [password, setPassword] = useState('');
-
-  // const userLogin = (e) => {
-  //   e.preventDefault();
-  //   const temp = {
-  //     email,
-  //     password,
-  //   };
-  //   fetch('/login', {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify(temp),
-  //   })
-  //     .then((res) => res.json())
-  //     .then((res) => {
-  //       console.log('response from post', res);
-  //       setEmail('');
-  //       setPassword('');
-  //     });
-  // };
   const onSuccess = (res) => {
     console.log('login success', res.profileObj);
     console.log('email', res.profileObj.email);
@@ -37,8 +14,9 @@ const LoginButton = () => {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({ email: res.profileObj.email, password: '0000' }),
+    }).then(() => {
+      location.reload();
     });
-
   };
 
   const onFailure = (res) => {
